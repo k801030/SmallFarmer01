2c07ffe0127994dd23d81ec7903388b15d1ba3b0
Vison <kk801030@gmail.com>
2015-09-11 21:34:05 +0800

    created OrdersActivity class

---
+++ app/src/main/java/edu/ntu/vison/smallfarmer01/activity/OrdersActivity.java
@@ -0,0 +1,17 @@
+package edu.ntu.vison.smallfarmer01.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.ntu.vison.smallfarmer01.R;
+
+/**
+ * Created by Vison on 2015/9/11.
+ */
+public class OrdersActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders);
+    }
+}
