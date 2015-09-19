b7211b6e83e8c657c6d7788b988ef6b0fe02fb21
Vison <kk801030@gmail.com>
2015-09-19 15:48:58 +0800

    finished instanceIdListener

--- app/src/main/java/edu/ntu/vison/smallfarmer01/service/MyInstanceIDListenerService.java
+++ app/src/main/java/edu/ntu/vison/smallfarmer01/service/MyInstanceIDListenerService.java
@@ -1,9 +1,23 @@
 package edu.ntu.vison.smallfarmer01.service;
 
+import android.content.Intent;
+
 import com.google.android.gms.iid.InstanceIDListenerService;
 
 /**
  * Created by Vison on 2015/9/19.
  */
 public class MyInstanceIDListenerService extends InstanceIDListenerService {
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. This call is initiated by the
+     * InstanceID provider.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        Intent intent = new Intent(this, RegistrationIntentService.class);
+        startService(intent);
+    }
+    // [END refresh_token]
 }
