f60ece8ba6bd5d1326522e51b70fc2372395df2f
Vison <kk801030@gmail.com>
2015-09-30 19:14:03 +0800

    create user data

---
+++ app/src/main/java/edu/ntu/vison/smallfarmer01/model/UserData.java
@@ -0,0 +1,24 @@
+package edu.ntu.vison.smallfarmer01.model;
+
+/**
+ * Created by Vison on 2015/9/30.
+ */
+public class UserData {
+    class User {
+        String last_name;
+        String first_name;
+    }
+
+    String user_avatar;
+    User user;
+
+    // Getter
+    public String getUserName() {
+        return user.last_name+user.first_name;
+    }
+
+    public String getUserImgUrl() {
+        return user_avatar;
+    }
+
+}
